2940bf9d4de9c46a810e4dd170be46e69d1ac0b0
qa-qmfto-4-gweq@example.com <qa-qmfto-4-gweq@example.com>
2023-05-12 23:21:29 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16341.stoplight-dev.com/api/v1/projects/cHJqOjE2Nzc/spectral.js?branch_slug=main&token=8dbff9a5-cf7f-4088-8cfb-5e92224a1a33";
+export default { extends: ruleset };
